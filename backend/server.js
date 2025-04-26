7ba7ee983e0fdf409887d7d2b81ee52ff199b3cf
Gurleen Rekhi <gurleenr@buffalo.edu>
2025-04-25 23:05:13 -0400

    adding rate limiting 50 reqs per 5 mins

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import rateLimit from 'express-rate-limit';
+
 
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import friendRoutes from './routes/friendRoutes.js';
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 50,                 // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 5 minutes'
+  });
 
 dotenv.config();
 const app = express();
@@ -14,6 +22,8 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
+
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
